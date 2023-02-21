e80de4275562d674d977e645bcbbc3449432a525
李易斯杰 <3376809259@qq.com>
2023-02-21 20:29:43 +0800

    提交

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-let snakeBody = [{x:0,y:0}] // 蛇
-let orientation = undefined // 蛇头朝向
-let snake = document.getElementById("snake")
+let snakeBody = [{ x : 50, y : 50 },{ x : 75, y : 50 },{ x : 100, y : 50}]; // 蛇
+let orientation = undefined; // 蛇头朝向
+let foodPosition = { x : 20, y : 20 };
+let map = document.getElementById("map");
+
+window.onload = function (){
+    drawFood()
+}
+
+// 刷新地图
+function updateMap(){
+    let children = map.children
+    for (let i = children.length - 1;i >=0; i--){
+        map.removeChild(children[i])
+    }
+    drawSnake()
+}
 
 // 绘制蛇身
 function drawSnake(){
     for (let i = 0; i <snakeBody.length; i++){
-        snake.style.bottom = (snakeBody[i].y ) + "px"
-        snake.style.left = (snakeBody[i].x ) + "px"
+        let snake = document.createElement("div")
+        snake.classList.add("snake");
+        snake.style.bottom = (snakeBody[i].y ) + "px";
+        snake.style.left = (snakeBody[i].x ) + "px";
+        map.appendChild(snake)
     }
-    move()
 }
 // 蛇的移动
 function move(){
@@ -27,10 +43,12 @@ function move(){
             head.x--
             break;
     }
+    snakeBody.unshift(head)
+    snakeBody.pop()
+    updateMap()
 }
 // 蛇头的朝向
 function moveTo(event){
-    console.log(event)
     if (event.key === "ArrowUp" && orientation !== "down"){
         //向上运动
         orientation = "up"
@@ -45,7 +63,20 @@ function moveTo(event){
         orientation = "right"
     }
 }
+
+// 生成食物
+function drawFood(){
+
+    let food = document.createElement("span")
+    foodPosition.x = Math.floor(Math.random() * map.offsetWidth);
+    foodPosition.y = Math.floor(Math.random() * map.offsetHeight);
+    food.className = "food"
+    food.style.left = foodPosition.x + "px"
+    food.style.bottom = foodPosition.y + "px"
+    map.appendChild(food)
+    console.log(foodPosition)
+}
     window.addEventListener('keydown',moveTo)
-    window.setInterval(drawSnake,1)
+    window.setInterval(move,1)
 
 // ArrowUp ArrowRight ArrowDown ArrowLeft
